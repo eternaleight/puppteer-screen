e9620a8667325ad4ed791ec12a826ac88598146a
eternaleight <eternaleight0@gmail.com>
2024-06-11 16:06:58 +0900

    fix

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const puppeteer_1 = __importDefault(require("puppeteer"));
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const browser = yield puppeteer_1.default.launch({ headless: true });
+    const page = yield browser.newPage();
+    // ビューポートの設定
+    yield page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 2, // デバイススケールファクターを2に設定
+    });
+    yield page.goto('https://github.com/eternaleight');
+    yield page.screenshot({ path: 'screenshot.png', fullPage: true }); // fullPageオプションで全体のスクリーンショットを取得
+    yield browser.close();
+}))();
